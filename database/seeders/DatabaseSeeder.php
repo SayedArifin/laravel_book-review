1fd5a392c85de2554bdc322221f14136e91d1cb7
Md Sayed Arifin <zinnatsaverdhaka@gmail.com>
2023-08-05 11:47:05 +0600

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
+use App\Models\Review;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Book::factory(34)->create()->each(function ($book) {
+            $numOfReviews = random_int(5, 30);
+            Review::factory()->count($numOfReviews)->good()->for($book)->create();
+        });
+        Book::factory(33)->create()->each(function ($book) {
+            $numOfReviews = random_int(5, 30);
+            Review::factory()->count($numOfReviews)->avg()->for($book)->create();
+        });
+        Book::factory(33)->create()->each(function ($book) {
+            $numOfReviews = random_int(5, 30);
+            Review::factory()->count($numOfReviews)->bad()->for($book)->create();
+        });
+    }
+}
